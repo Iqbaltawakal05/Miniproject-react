45593bd8ebf55cde44544fde34742c35bde28596
iqbaltawakal1205 <iqbaltawakall006@gmail.com>
2024-03-06 15:19:40 +0700

    Get list user from API

--- Pages/User.jsx
+++ Pages/User.jsx
@@ -3,22 +3,30 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 function User () {
-    const [User, setUser] = useState({})
+    const [User, setUser] = useState([])
 
     const getUserdata = () => {
         axios.get('https://reqres.in/api/users?page=2')
-        .then((res) => console.log(res))
+        .then((res) => setUser(res.data.data))
         .catch((err) => console.log(err))
     }
 
     useEffect(() => {
         getUserdata()
     }, [])
-    
+
     return (
         <div>
             <Navbar />
             <h1>User</h1>
+            {User.map((item) => (
+                    <div>
+                        <h1>{item.first_name}</h1>
+                        <p>{item.email}</p>
+                        <img src={item.avatar} alt=""/>
+                    </div>
+                ))
+            }
         </div>
     )
 }
