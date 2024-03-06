ab2400a89e177b93bede59ec8c93332e810e370a
iqbaltawakal1205 <iqbaltawakall006@gmail.com>
2024-03-06 15:07:11 +0700

    Test API

--- Pages/User.jsx
+++ Pages/User.jsx
@@ -1,6 +1,20 @@
 import Navbar from "../Components/Navbar"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function User () {
+    const [User, setUser] = useState({})
+
+    const getUserdata = () => {
+        axios.get('https://reqres.in/api/users?page=2')
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err))
+    }
+
+    useEffect(() => {
+        getUserdata()
+    }, [])
+    
     return (
         <div>
             <Navbar />
