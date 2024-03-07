344ede4f197c702548333a62d9436b0ff11383d8
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-03-07 10:58:48 +0700

    Test API register and Run API

--- Pages/Register.jsx
+++ Pages/Register.jsx
@@ -1,14 +1,36 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
 function Register () {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleRegister = () => {
+        const payload = {
+            email: email,
+            password: password,
+        }
+
+        axios.post('https://reqres.in/api/register', payload)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err))
+    }
+
+
     return (
         <div>
             <h1>Register</h1>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
-            <button>Register</button>
+            <input type="text" placeholder="Email" onChange={handleEmailChange}/>
+            <input type="password" placeholder="Add Password" onChange={handlePasswordChange}/>
+            <button onClick={handleRegister}>Register</button>
             <Link to={'/login'}>
                 <button>Login</button>
             </Link>
