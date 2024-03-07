9dc2f4f17e37a133d60dcb6df90144075b1ee611
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-03-07 10:01:40 +0700

    Add login.jsx to pages folder

---
+++ Pages/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar"
+
+function Login () {
+    return (
+        <div>
+        <Navbar/>
+            <p>Login</p>
+        </div>
+    )
+}
+
+export default Login
