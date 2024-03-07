017f63e5f5f2ca9059ed9ec89d418c4c8af4711a
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-03-07 13:27:43 +0700

    And logout button and navigate in home page

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -5,7 +5,6 @@ function Navbar () {
         <div>
             <Link to={"/"}><p>Home</p></Link>
             <Link to={"/user"}>User</Link>
-            <Link to={"/login"}>Login</Link>
         </div>
     )
 }
--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,9 +1,24 @@
-import Navbar from "../Components/Navbar"
-
+import Navbar from "../Components/Navbar" 
+import { useNavigate } from 'react-router-dom';
 function Home () {
+    const Navigate = useNavigate();
+    const token = localStorage.getItem('access_token')
+
+    const logout = () => {
+        localStorage.removeItem('access_token')
+        Navigate('/login')
+    }
+
     return (
         <div>
             <Navbar />
+            {token ? (
+                <button onClick={logout}>Logout</button>
+            ) : (
+                <Link to={'/login'}>
+                    <h1>Login</h1>
+                </Link>
+            )}
             <h1>Home</h1>
         </div>
     )
