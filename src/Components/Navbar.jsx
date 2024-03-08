43b0285af4a4cd6589780336f6ffcff6b0cd84c0
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-03-08 23:04:27 +0700

    Add style.css and Navbar style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,26 @@
-
+import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
+import "../Css/Style.css"
 function Navbar () {
+    const Navigate = useNavigate();
+    const token = localStorage.getItem('access_token')
+
+    const logout = () => {
+        localStorage.removeItem('access_token')
+        Navigate('/login')
+    }
+
     return (
-        <div>
-            <Link to={"/"}><p>Home</p></Link>
+        <div className="Navbar">
+            <Link to={"/"}><a href="">Home</a></Link>
             <Link to={"/user"}>User</Link>
+            {token ? (
+                <button onClick={logout}>Logout</button>
+            ) : (
+                <Link to={'/login'}>
+                    <button>Login</button>
+                </Link>
+            )}
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,10 @@
-import Navbar from "../Components/Navbar" 
+import Layout from "../Components/Layout"
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import Layout from "../Components/Layout"
 function Home () {
-    const Navigate = useNavigate();
-    const token = localStorage.getItem('access_token')
-
-    const logout = () => {
-        localStorage.removeItem('access_token')
-        Navigate('/login')
-    }
 
     return (
         <Layout>
-            {token ? (
-                <button onClick={logout}>Logout</button>
-            ) : (
-                <Link to={'/login'}>
-                    <h1>Login</h1>
-                </Link>
-            )}
             <h1>Home</h1>
         </Layout>
     )
